dab7b7671c7b514d8e6d2f076237d4d262354834
rickdastardly <71228459+rickdastardly@users.noreply.github.com>
2022-01-06 18:48:14 +0000

    Add files via upload

---
+++ Mini First Person Controller/Scripts/Components/Player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Vairables
+    public float maxHealth, maxThirst, maxHunger;
+    public float thirstDecreaseRate, hungerDecreaseRate, healthDecreaseRate;
+    public float health, thirst, hunger;
+
+    public bool dead;
+
+    //Functions
+    public void Start()
+    {
+        health = maxHealth;
+    }
+
+
+    public void Update()
+    {
+        //Thirst and hunger decrease over time
+        if (!dead)
+        {
+            thirst += thirstDecreaseRate * Time.deltaTime;
+            hunger += hungerDecreaseRate * Time.deltaTime;
+        }
+
+
+
+        if (thirst >= maxThirst)
+        {
+            healthDecreaseRate = healthDecreaseRate + 0.25;
+        }
+
+
+        if (hunger >= maxHunger)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        dead = true;
+        print("Player has died.");
+    }
+}
